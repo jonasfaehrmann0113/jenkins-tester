6b70b1fae0db7d3d9e89f5091c478c227f1c930f
jonasfaehrmann0113 <45172815+jonasfaehrmann0113@users.noreply.github.com>
2019-03-04 10:37:12 +0100

    Added NonCPS to avoid notSerializableException due to currentBuild.getRawBuild

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,16 +2,20 @@ import hudson.model.ParametersAction
 import hudson.model.ParameterValue
 import hudson.model.StringParameterValue
 
-def buildVersionParam = new StringParameterValue('BUILD_VERSION', "${currentBuild.startTimeInMillis}", "The build version.")
-def build = currentBuild.getRawBuild();
-build.actions.add(new ParametersAction(buildVersionParam))
-
 try {
     node {
         timestamps {
+			setBuildVersion()
 			echo "test"
 		}
 	}
 } catch (error) {
 	println error
+}
+
+@NonCPS
+def setBuildVersion(){
+	def buildVersionParam = new StringParameterValue('BUILD_VERSION', "${currentBuild.startTimeInMillis}", "The build version.")
+	def build = currentBuild.getRawBuild();
+	build.actions.add(new ParametersAction(buildVersionParam))
 }
