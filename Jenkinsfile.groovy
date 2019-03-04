a695968453d624d0388c7c112ccbbdab9957e69d
jonasfaehrmann0113 <45172815+jonasfaehrmann0113@users.noreply.github.com>
2019-03-04 13:57:29 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,7 +16,7 @@ try {
 	}
 if (Gatekeeper.isGateOpen()) {
         println "Gate is open. Starting production deployment."
-        build job: "${NAME}-prod", parameters: [string(name: "BUILD_VERSION", value: BUILD_VERSION)], wait: false
+	build job: "${NAME}-prod", parameters: [string(name: "BUILD_VERSION", value: BUILD_VERSION], wait: false
     } else {
         println "Gate is closed."
 }
@@ -26,7 +26,7 @@ if (Gatekeeper.isGateOpen()) {
 
 @NonCPS
 def setBuildVersion(){
-	def buildVersionParam = new StringParameterValue('BUILD_VERSION', "${currentBuild.startTimeInMillis}", "The build version.")
+	def buildVersionParam = new StringParameterValue('BUILD_VERSION', "1.${currentBuild.startTimeInMillis}", "The build version.")
 	def build = currentBuild.getRawBuild();
 	build.actions.add(new ParametersAction(buildVersionParam))
 }
