1884dd65dedb348ae08c55ecc58cfa5305e97279
jonasfaehrmann0113 <45172815+jonasfaehrmann0113@users.noreply.github.com>
2019-03-04 10:47:09 +0100

    Added GateKeeper

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,25 @@
+@Library("gatekeeper-library")
+import de.otto.plm.Gatekeeper
 import hudson.model.ParametersAction
 import hudson.model.ParameterValue
 import hudson.model.StringParameterValue
+
+def NAME = 'dev-jfaehrma-test'
 
 try {
     node {
         timestamps {
 			setBuildVersion()
 			echo "test"
+			echo BUILD_VERSION
 		}
 	}
+if (Gatekeeper.isGateOpen()) {
+        println "Gate is open. Starting production deployment."
+        build job: "${NAME}-prod", parameters: [string(name: "BUILD_VERSION", value: BUILD_VERSION)], wait: false
+    } else {
+        println "Gate is closed."
+}
 } catch (error) {
 	println error
 }
